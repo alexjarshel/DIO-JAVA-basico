edc3735a117113519f23c02e36398374ffe87e8e
Alex Jarschel <alexbaukartjarschel@gmail.com>
2024-09-02 21:17:42 -0300

    Orientação a Objetos

---
+++ DIO/Orintação a Objetos/Orientação a Objetos/Orientação a Objetos/src/School.java
@@ -0,0 +1,15 @@
+public class School {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "alex";
+        student1.age = 22;
+
+        Student student2 = new Student();
+        student2.name = "Marcos";
+        student2.age = 20;
+
+        student1.eating();
+    }
+    
+}
---
+++ DIO/Orintação a Objetos/Orientação a Objetos/Orientação a Objetos/src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    String name;   
+    int age;
+    
+    void eating(){
+        System.out.println(name + " is eating...");
+    }
+
+    void drinking(){
+        System.out.println(name + " is drinking...");
+
+    }
+
+    void runing(){
+        System.out.println(name + " is runing...");
+
+    }
+}
