ea6846e24d45dab568d73bf27b746e9ea1d76db6
Alex Jarschel <alexbaukartjarschel@gmail.com>
2024-09-08 20:19:17 -0300

    bank

---
+++ DIO/Pilares da OO/exercicios/Bank/src/CheckingAccount.java
@@ -0,0 +1,27 @@
+public class CheckingAccount {
+    
+    private int bankBranch;
+    private int accountNumber;
+    private double balance;
+
+    public int getBankBranch() {
+        return bankBranch;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit() {
+    }
+
+    public void draw() {
+    }
+
+    public void transfer() {
+    }
+}
---
+++ DIO/Pilares da OO/exercicios/Bank/src/SavingsAccount.java
@@ -0,0 +1,28 @@
+public class SavingsAccount {
+
+    private int bankBranch;
+    private int accountNumber;
+    private double balance;
+
+    public int getBankBranch() {
+        return bankBranch;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit() {
+    }
+
+    public void draw() {
+    }
+
+    public void transfer() {
+    }
+
+}
