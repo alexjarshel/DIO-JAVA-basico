e44b54b3c2740be5ac7e6b2d9f254ceff4cb291f
Alex Jarschel <alexbaukartjarschel@gmail.com>
2024-09-21 12:18:53 -0300

    Listas

---
+++ DIO/Estrutura de dados/Estrutura de dados/src/listas/No.java
@@ -0,0 +1,46 @@
+package listas;
+
+public class No<T> {
+    private T conteudo;
+    private No<T> prox = null;
+
+    public No (){
+        this.prox = null;
+    }
+
+    public No (T conteudo){
+        this.prox = null;
+        this.conteudo = conteudo;
+    }
+
+    public Object getConteudo() {
+        return conteudo;
+    }   
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public No<T> getProx() {
+        return prox;
+    }
+    public void setProx(No<T> prox) {
+        this.prox = prox;
+    }
+
+    @Override
+    public String toString(){
+        return "{conteudo : " + this.conteudo +"}";
+    }
+
+    public String toStringEncadeado(){
+        String  str = "No{" +
+                "Conteudo : " + this.conteudo + 
+                "}";
+        if(prox != null){
+            str += "->" + prox.toString();
+        }else{
+            str += "-> null";
+        }
+        return str;
+    }
+}
